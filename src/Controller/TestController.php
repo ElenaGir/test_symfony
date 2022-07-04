7f5b7536206c6054df204e0158239370e0c71192
ElenaGir <elena.girard@gmail.com>
2022-07-04 12:44:01 +0200

    premieres pages

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -12,14 +12,23 @@ class TestController
     {
         dd("ca fonctionne");
     }
+// une autre façon de configurer une route sans passer par le ficher .yaml est ecrire dans le commentaire commz ci-dessus
+// avec @Route (avec majusqule). 
+// pour que symfony puisse gerer le données en commentaires il faut installer des packedges supplementaires symfony flex
+
 
     /**
      * @Route("/test/{age<\d+>?0}", name="test", methods={"GET", "POST"}, host="localhost", schemes={"http", "https"})
      */
+
+
+    // click droit sut @Route et choisir import class on propose 2, il faut choisir celle avec Annotation.
+    // cela nous mettra en olace le bon "use"
     public function  test(Request $request, $age)
     {  
         dump($request);
-        // $age = $request->attributes->get('age');
+        
+        //  soit on ecrit cette ligne: $age = $request->attributes->get('age'); soit on passe $age directement dans les parametres de la function
         dd("vous avez $age ans");
     }
 }
